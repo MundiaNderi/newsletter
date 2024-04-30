8ba349bfb55d072c72ba4b109bdf66f0c67a3175
Serah Nderi <sarahngima77@gmail.com>
2024-04-30 23:25:27 +0300

    Form validation and inputs

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const sucessModal = document.getElementById("success");
+  console.log(success)
 
   const cardNews = document.getElementById("card");
 
@@ -10,22 +11,43 @@ document.addEventListener("DOMContentLoaded", () => {
     cardNews.classList.add("hidden");
     sucessModal.classList.remove("hidden");
   });
+
+
 });
 
+// Validate the form 
 function validateEmail() {
-  const emailInput = document.getElementById("contact-email").value;
+  const emailInput = document.getElementById("contact-email").value.trim();
   const emailError = document.getElementById("email-error");
+  const emailRegex = /^[A-Za-z\._\-0-9]+@[A-Za-z]+\.[a-z]{2,4}$/;
 
-  if (emailInput.length == 0) {
+  if (emailInput.length === 0) {
     emailError.textContent = "Email is required";
     return false;
-  }
-
-  if (!emailInput.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+  } else if (!emailRegex.test(emailInput)) {
     emailError.textContent = "Valid email required";
     return false;
+  } else {
+    emailError.textContent = "";
+    const encodedEmail = encodeURIComponent(emailInput);
+    const redirectURL = `index.html?email=${encodedEmail}`; 
+    window.history.pushState({ path: redirectURL }, "", redirectURL); // Update the URL
+    document.getElementById("subscriber-email").textContent = emailInput;
+    return true;
   }
-
-  emailError.textContent = ""
-  return true;
 }
+
+
+// Dismiss the success card
+document.addEventListener('DOMContentLoaded', function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const userEmail = urlParams.get("email");
+  const dismiss = document.getElementById("success");
+
+  // Display the email on the success page
+  document.getElementById("subscriber-email").textContent = userEmail;
+
+  dismiss.addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+});
